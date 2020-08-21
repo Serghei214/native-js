319830c133ce4673c4dcd26b90af52a04fb0ee6f
Serghei214 <Serghei214@users.noreply.github.com>
2020-08-21 15:35:20 -0700

    intro to test driven development

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {splitIntoWords} from './01-hello-tests/01';
+
+const sentence = "Hello my friend";
+const result = splitIntoWords(sentence);
+console.log(result[0] === "hello");
+console.log(result[1] === "my");
+console.log(result[2] === "friend");
+
 
 ReactDOM.render(
   <React.StrictMode>
